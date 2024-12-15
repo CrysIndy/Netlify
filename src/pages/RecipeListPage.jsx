e946f4c39398aa9a1b1190be0d7831dc4db29bf0
Marco Stefanutto <crystalindy@kpnmail.nl>
2024-12-15 23:21:39 +0100

    second try updated header text

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -31,7 +31,7 @@ export const RecipeListPage = ({recipes, onSelectRecipe}) => {
 				flexDir={{base: "column", sm: "row"}}
 				color="gray.300"
 			>
-				<Heading p="1rem">Winc Recipe Finder</Heading>
+				<Heading p="1rem">Winc Recipe Finder updated</Heading>
 				<RecipeSearch onSearch={handleSearch} />
 			</Center>
 
